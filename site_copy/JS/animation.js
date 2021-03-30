8a2993fd48f2687cc70a079088459d461fd8335b
hideaki.ogino <hideaki.meido.YUN11503@gmail.com>
2021-03-31 00:17:25 +0900

    総合演習

---
+++ site_copy/JS/animation.js
@@ -0,0 +1,182 @@
+$(function() {
+    //スクロールしてページトップから550に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 550) {
+            $('.INTRODUCTION').addClass('active');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから550に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 550) {
+            $('.INTRODUCTION .black').addClass('anime2_01');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから550に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 550) {
+            $('.INTRODUCTION .black_ber').addClass('anime2_02');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから550に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 550) {
+            $('.INTRODUCTION .p_black').addClass('anime2_03');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから1000に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 1000) {
+            $('.pure .boxs').addClass('anime3');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから1000に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 1000) {
+            $('.pure .boxs white_box').addClass('anime3_1');
+        }
+    });
+});
+
+
+$(function() {
+    //スクロールしてページトップから2400に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 2400) {
+            $('.here .box .white_box').addClass('anime4_1');
+        }
+    });
+});
+
+
+
+$(function() {
+    //スクロールしてページトップから2400に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 2850) {
+            $('.our .block').addClass('anime5_1');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから2400に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 2850) {
+            $('.our_img img').addClass('anime5_2');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから3200に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 3200) {
+            $('.ABOUT .black').addClass('anime6_1');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから3200に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 3200) {
+            $('.ABOUT .p_black').addClass('anime6_2');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから4200に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 4200) {
+            $('.why .black').addClass('anime7_1');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから4200に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 4200) {
+            $('.why .black_ber').addClass('anime7_2');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから4200に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 4200) {
+            $('.why .p_black').addClass('anime7_3');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから4700に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 4700) {
+            $('.get .boxs').addClass('anime8');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから5200に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 5200) {
+            $('.TOUCH .black').addClass('anime9_1');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから5200に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 5200) {
+            $('.TOUCH .black_ber').addClass('anime9_2');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから5200に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 5200) {
+            $('.TOUCH .p_black').addClass('anime9_3');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから5700に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 5700) {
+            $('#foot .left').addClass('anime_left');
+        }
+    });
+});
+
+$(function() {
+    //スクロールしてページトップから5700に達したらクラスを付与する
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 5700) {
+            $('#foot .right').addClass('anime_right');
+        }
+    });
+});
